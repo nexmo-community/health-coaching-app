21dd6ffcf1dc91b0d7d4ac830daeca08063cd476
NJalal7 <nahrin.jalal@vonage.com>
2020-02-20 17:33:46 -0500

    saving

--- src/routes/contact/index.js
+++ src/routes/contact/index.js
@@ -10,8 +10,9 @@ const photographs = (props) => {
 					<div>Let's connect.</div>
 					<div>&nbsp;</div>
 				</p>
-				<div id="otEmbedContainer" style="width:800px; height:640px"></div>
-				<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=7e090103-ae17-4dc7-b610-ada90089c782&room=DEFAULT_ROOM"></script>
+				<div class={style.otEmbedContainer}>
+					<script src="https://tokbox.com/embed/embed/ot-embed.js?embedId=7e090103-ae17-4dc7-b610-ada90089c782&room=DEFAULT_ROOM"></script>
+				</div>
 			</div>
 		</div>
 	);
